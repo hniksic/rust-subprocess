99fef10e3ae1cd41785654f99fca6cf77c91746a
Hrvoje Niksic <hniksic@gmail.com>
2016-12-12 00:13:41 +0100

    Improve error checking.

--- src/popen.rs
+++ src/popen.rs
@@ -1,8 +1,7 @@
 use std::path::{PathBuf, Path};
 use std::io::{Result, Error, Read, Write};
 use std::mem;
-
-use std::os::unix::io::AsRawFd;
+use std::fs::File;
 
 use posix;
 pub use posix::{SIGKILL, SIGTERM, ExitStatus};
@@ -15,7 +14,9 @@ pub struct Popen {
 }
 
 
-fn set_cloexec(fd: i32) -> Result<()> {
+fn set_cloexec(f: &File) -> Result<()> {
+    use std::os::unix::io::AsRawFd;
+    let fd = f.as_raw_fd();
     let old = try!(posix::fcntl(fd, posix::F_GETFD, None));
     posix::fcntl(fd, posix::F_SETFD, Some(old | posix::FD_CLOEXEC))
         .and(Ok(()))
@@ -37,26 +38,25 @@ impl Popen {
 
     fn start(&mut self) -> Result<()> {
         let mut exec_fail_pipe = try!(posix::pipe());
-        set_cloexec(exec_fail_pipe.0.as_raw_fd()).unwrap();
-        set_cloexec(exec_fail_pipe.1.as_raw_fd()).unwrap();
+        try!(set_cloexec(&exec_fail_pipe.0));
+        try!(set_cloexec(&exec_fail_pipe.1));
         let child_pid = try!(posix::fork());
         if child_pid == 0 {
             mem::drop(exec_fail_pipe.0);
             let result = posix::execvp(&self.args[0], &self.args);
+            // Notify the parent process that exec has failed, and exit.
             let error_code: i32 = match result {
                 Ok(()) => unreachable!(),
                 Err(e) => e.raw_os_error().unwrap_or(-1)
             };
-            // XXX we don't really need formatting here - we could use
-            // the byteorder crate to communicate the i32 over the
-            // pipe
+            // XXX use the byteorder crate to serialize the error
             exec_fail_pipe.1.write_all(format!("{}", error_code).as_bytes()).unwrap();
             posix::_exit(127);
         }
         self.pid = Some(child_pid as u32);
         mem::drop(exec_fail_pipe.1);
         let mut error_string = String::new();
-        exec_fail_pipe.0.read_to_string(&mut error_string).unwrap();
+        try!(exec_fail_pipe.0.read_to_string(&mut error_string));
         if error_string.len() != 0 {
             let error_code: i32 = error_string.parse().unwrap();
             Err(Error::from_raw_os_error(error_code))
