9f5bd7635adafb3d30e052d90563593622166bb3
Hrvoje Niksic <hniksic@gmail.com>
2016-12-11 21:42:10 +0100

    Communicate failure to exec to the parent.

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,13 @@ mod tests {
     use popen::Popen;
 
     #[test]
-    fn new() {
+    fn good_cmd() {
         let p = Popen::create(&["ls", "-al"]).unwrap();
-        println!("{:?}", p);
+    }
+
+    #[test]
+    fn bad_cmd() {
+        let result = Popen::create(&["nosuchcommand"]);
+        assert!(result.is_err());
     }
 }
--- src/popen.rs
+++ src/popen.rs
@@ -1,26 +1,58 @@
 use std::path::{PathBuf, Path};
-use std::io::{Result, Error};
+use std::io::{Result, Error, Read, Write};
+use std::mem;
 
-use libc::*;
-use std::os::unix::ffi::OsStrExt;
-use std::ptr;
+use libc;
 
 #[derive(Debug)]
 pub struct Popen {
     args: Vec<PathBuf>,
-    pid: Option<pid_t>,
+    pid: Option<libc::pid_t>,
 }
 
-fn check_err<T: Ord + Default>(num: T) -> Result<T> {
-    if num < T::default() {
-        return Err(Error::last_os_error());
+mod wrapped {
+    use std::io::{Result, Error};
+    use std::path::Path;
+    use libc;
+    use std::os::unix::ffi::OsStrExt;
+    use std::fs::File;
+    use std::os::unix::io::FromRawFd;
+    use std::ptr;
+
+    fn check_err<T: Ord + Default>(num: T) -> Result<T> {
+        if num < T::default() {
+            return Err(Error::last_os_error());
+        }
+        Ok(num)
     }
-    Ok(num)
-}
 
-fn path_as_ptr(p: &Path) -> *const c_char {
-    let c_bytes = p.as_os_str().as_bytes();
-    &c_bytes[0] as *const u8 as *const c_char
+    fn path_as_ptr(p: &Path) -> *const libc::c_char {
+        let c_bytes = p.as_os_str().as_bytes();
+        &c_bytes[0] as *const u8 as *const libc::c_char
+    }
+
+    pub fn pipe() -> Result<(File, File)> {
+        let mut fds = [0 as libc::c_int; 2];
+        try!(check_err(unsafe { libc::pipe(&mut fds[0]) }));
+        Ok(unsafe {
+            (File::from_raw_fd(fds[0]), File::from_raw_fd(fds[1]))
+        })
+    }
+
+    pub fn fork() -> Result<libc::pid_t> {
+        check_err(unsafe { libc::fork() })
+    }
+
+    pub fn execvp<P1, P2>(cmd: P1, args: &[P2]) -> Result<()>
+        where P1: AsRef<Path>, P2: AsRef<Path> {
+        let mut args_os: Vec<_> = args.iter()
+            .map(|x| path_as_ptr(x.as_ref())).collect();
+        args_os.push(ptr::null());
+        let argv = &args_os[0] as *const *const libc::c_char;
+        check_err(unsafe { libc::execvp(path_as_ptr(cmd.as_ref()), argv) })
+            .map(|_| ())
+    }
+
 }
 
 impl Popen {
@@ -36,16 +68,29 @@ impl Popen {
     }
 
     fn start(&mut self) -> Result<()> {
-        let child_pid = try!(check_err(unsafe { fork() }));
+        let mut exec_fail_pipe = try!(wrapped::pipe());
+        let child_pid = try!(wrapped::fork());
         if child_pid == 0 {
-            let cmd = &self.args[0];
-            let mut args_os: Vec<_> = self.args.iter()
-                .map(|x| path_as_ptr(x)).collect();
-            args_os.push(ptr::null());
-            let argv = &args_os[0] as *const *const c_char;
-            check_err(unsafe { execvp(path_as_ptr(cmd), argv) }).unwrap();
+            mem::drop(exec_fail_pipe.0);
+            let result = wrapped::execvp(&self.args[0], &self.args);
+            let error_code: i32 = match result {
+                Ok(()) => unreachable!(),
+                Err(e) => e.raw_os_error().unwrap_or(-1)
+            };
+            // XXX we don't really need formatting here - just send
+            // off the 4-byte content of the variable over the pipe
+            exec_fail_pipe.1.write_all(format!("{}", error_code).as_bytes()).unwrap();
+            unsafe { libc::_exit(127) }
         }
         self.pid = Some(child_pid);
-        Ok(())
+        mem::drop(exec_fail_pipe.1);
+        let mut error_string = String::new();
+        exec_fail_pipe.0.read_to_string(&mut error_string).unwrap();
+        if error_string.len() != 0 {
+            let error_code: i32 = error_string.parse().unwrap();
+            Err(Error::from_raw_os_error(error_code))
+        } else {
+            Ok(())
+        }
     }
 }
