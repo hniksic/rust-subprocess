52cd94d1fbfe01fb07c5a6cc59463323e1836411
Hrvoje Niksic <hniksic@gmail.com>
2016-12-23 18:12:26 +0100

    Run more tests under Win32.

--- src/lib.rs
+++ src/lib.rs
@@ -93,24 +93,6 @@ mod tests_common {
     }
 
     #[test]
-    fn communicate_input() {
-        let tmpdir = TempDir::new("test").unwrap();
-        let tmpname = tmpdir.path().join("input");
-        let mut p = Popen::create_full(
-            &["cat"],
-            Redirection::Pipe,
-            Redirection::File(File::create(&tmpname).unwrap()),
-            Redirection::None)
-            .unwrap();
-        if let (None, None) = p.communicate_bytes(Some(b"hello world")).unwrap() {
-        } else {
-            assert!(false);
-        }
-        assert!(p.wait().unwrap() == Some(ExitStatus::Exited(0)));
-        assert!(read_whole_file(File::open(&tmpname).unwrap()) == "hello world");
-    }
-
-    #[test]
     fn input_output_from_file() {
         let tmpdir = TempDir::new("test").unwrap();
         let tmpname_in = tmpdir.path().join("input");
@@ -128,42 +110,23 @@ mod tests_common {
         assert!(p.wait().unwrap() == Some(ExitStatus::Exited(0)));
         assert!(read_whole_file(File::open(&tmpname_out).unwrap()) == "foo");
     }
-}
-
-#[cfg(all(test, unix))]
-mod tests_unix {
-    extern crate tempdir;
-
-    use subprocess::{Popen, ExitStatus, Redirection};
-    use std::fs::File;
-    use std::io::Write;
-    use std::mem;
-    use libc::SIGTERM;
-
-    use self::tempdir::TempDir;
-
-    use tests_common::read_whole_file;
 
     #[test]
-    fn err_signal() {
-        let mut p = Popen::create(&["sleep", "5"]).unwrap();
-        assert!(p.poll().is_none());
-        p.terminate().unwrap();
-        assert!(p.wait().unwrap() == Some(ExitStatus::Signaled(SIGTERM as u8)));
-    }
-
-    #[test]
-    fn write_to_stdin() {
+    fn communicate_input() {
         let tmpdir = TempDir::new("test").unwrap();
-        let tmpname = tmpdir.path().join("output");
+        let tmpname = tmpdir.path().join("input");
         let mut p = Popen::create_full(
-            &["dd".to_string(), format!("of={}", tmpname.display()), "status=none".to_string()],
-            Redirection::Pipe, Redirection::None, Redirection::None)
+            &["cat"],
+            Redirection::Pipe,
+            Redirection::File(File::create(&tmpname).unwrap()),
+            Redirection::None)
             .unwrap();
-        p.stdin.as_mut().unwrap().write_all(b"foo").unwrap();
-        mem::drop(p.stdin.take());
+        if let (None, None) = p.communicate_bytes(Some(b"hello world")).unwrap() {
+        } else {
+            assert!(false);
+        }
         assert!(p.wait().unwrap() == Some(ExitStatus::Exited(0)));
-        assert!(read_whole_file(File::open(tmpname).unwrap()) == "foo");
+        assert!(read_whole_file(File::open(&tmpname).unwrap()) == "hello world");
     }
 
     #[test]
@@ -225,6 +188,43 @@ mod tests_unix {
             assert!(false);
         }
         assert!(p.wait().unwrap() == Some(ExitStatus::Exited(0)));
+    }
+}
+
+#[cfg(all(test, unix))]
+mod tests_unix {
+    extern crate tempdir;
+
+    use subprocess::{Popen, ExitStatus, Redirection};
+    use std::fs::File;
+    use std::io::Write;
+    use std::mem;
+    use libc::SIGTERM;
+
+    use self::tempdir::TempDir;
+
+    use tests_common::read_whole_file;
+
+    #[test]
+    fn err_signal() {
+        let mut p = Popen::create(&["sleep", "5"]).unwrap();
+        assert!(p.poll().is_none());
+        p.terminate().unwrap();
+        assert!(p.wait().unwrap() == Some(ExitStatus::Signaled(SIGTERM as u8)));
+    }
+
+    #[test]
+    fn write_to_subprocess() {
+        let tmpdir = TempDir::new("test").unwrap();
+        let tmpname = tmpdir.path().join("output");
+        let mut p = Popen::create_full(
+            &["dd".to_string(), format!("of={}", tmpname.display()), "status=none".to_string()],
+            Redirection::Pipe, Redirection::None, Redirection::None)
+            .unwrap();
+        p.stdin.as_mut().unwrap().write_all(b"foo").unwrap();
+        mem::drop(p.stdin.take());
+        assert!(p.wait().unwrap() == Some(ExitStatus::Exited(0)));
+        assert!(read_whole_file(File::open(tmpname).unwrap()) == "foo");
     }
 }
 
